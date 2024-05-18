14f384f6eb91a8f505546bffc48e29c9c4d1be02
Erin-Soares <127992261+Erin-Soares@users.noreply.github.com>
2024-05-18 15:05:04 -0300

    versionando as rotas

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+const routes = require("./routes/routes");
 
+const app = express();
 const port = 3000;
 
-app.get("/home", (req, res) => {
-  res.send("Hello World");
-});
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 app.listen(3000, () => {
   console.log(`servidor rodando em http://localhost:${port}`);
