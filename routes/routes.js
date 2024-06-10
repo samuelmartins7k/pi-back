f569ce1f3745bc3771e13ce40a27991d7a928c6b
Samuel Martins <samuel.am709@gmail.com>
2024-06-09 23:17:08 -0300

    Atualizações de segurança

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const routes = require("express").Router();
-const TaskController = require("../controller/TaskController");
-
-routes.get("/", TaskController.getAllTasks);
-routes.post("/", TaskController.createTask)
-routes.put("/:id")
-
-module.exports = routes;
